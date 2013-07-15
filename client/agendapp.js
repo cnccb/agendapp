0b311c1d3c896730b319a77d3aa02384ef3eac02
jfalconnet <julien.falconnet@canne-et-dragons.org>
2013-07-15 22:23:21 +0200

    finitions sur le formulaire et inserion bd

--- client/agendapp.js
+++ client/agendapp.js
@@ -1,47 +1,49 @@
 //insert the new event
-  
-  Template.listeEvt.evenements = function () {
+
+Template.listeEvt.evenements = function() {
     return Evenements.find();
-  };
+};
 
-  Template.nouvelEvt.events({
-    'click #submitevt' : function (e) {
-      e.preventDefault();
-      var newId = _.uniqueId();
+Template.nouvelEvt.events({
+    'click #submitevt': function(e) {
+        e.preventDefault();
+        var newId = _.uniqueId();
 
 
-      var newEvent = 
-      {
-             admin: admin.value, //Mail pour l'administration
-             permalink: newId,
-            // password:'test',
-             nom: nom.value, //nom de l'évènement
-             date: date.value,// Date 
-            // forclusion: 20131110, //Date de forclusion
-            // horaires:'Samedi 10h00-19h00, Dimanche 10h00-17h00', //horaires (avec alternatives si pas bordé)
-            // echelle: 'Nationale', //Echelle (nationale, régionale, ...)
-            // cible:'des cannistes !', //Public attendu (->cible)
-            // prix:'30€', //Prix d'inscription
-            // orga: 'Julien Falconnet', //Nom de l'organisateur, 
-            // statut:'en cours', //Statut           
-            // contact:'webmaster@darkhan.net', //Contact (pour information)            
-             lieu: lieu.value, //Lieu / adresse
-            // plan:'http://goo.gl/maps/U6l2H', // Plan d’accès
-            // url:'http://www.internationaux2015.com',
-            // programme:'bla bla bla bla bla bla bla', // Programme
-            // options:['buvette','repas sur place'], //Options (buvette, offre de repas, etc.)
-            // conditions:'majeur et vacciné', //Conditions de participation
-            // hebergement:'non', //Recommandations d’hébergement
-            // restauration:'non', //Recommandations restauration
-            // visites:'bof' //Recommandations visites
-        };
+        var newEvent =
+                {
+                    admin: admin.value, //Mail pour l'administration
+                    permalink: newId,
+                    // password:'test',
+                    nom: nom.value, //nom de l'évènement
+                    datedeb: datedeb.value, // Date 
+                    datefin: datefin.value, // Date 
+                    forclusion: forclusion.value, //Date de forclusion
+                    horaires: horaires.value, //horaires (avec alternatives si pas bordé)
+                    echelle: echelle.value, //Echelle (nationale, régionale, ...)
+                    cible: cible.value, //Public attendu (->cible)
+                    prix: prix.value, //Prix d'inscription
+                    orga: orga.value, //Nom de l'organisateur, 
+                    statut: statut.value, //Statut           
+                    contact: contact.value, //Contact (pour information)            
+                    tel: tel.value, //tel orga
+                    lieu: lieu.value, //Lieu / adresse
+                    plan: plan.value, // Plan d’accès
+                    url: url.value,
+                    programme: programme.value, // Programme
+                    options: options.value, //Options (buvette, offre de repas, etc.)
+                    conditions: conditions.value, //Conditions de participation
+                    hebergement: hebergement.value, //Recommandations d’hébergement
+                    restauration: restauration.value, //Recommandations restauration
+                    visites: visites.value //Recommandations visites
+                };
         console.log("nouvel evènement");
         console.log(newEvent);
         Evenements.insert(newEvent);
 
     },
-    'submit' : function(e){
-      console.log('form submit');
-      e.preventDefault();
+    'submit': function(e) {
+        console.log('form submit');
+        e.preventDefault();
     }
-  });
+});
