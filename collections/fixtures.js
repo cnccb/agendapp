a948ddc9176730c24703a71351c2cb6b53d2e739
breucker <breucker@ifris.org>
2013-07-18 00:19:19 +0200

    fichier fixtures

---
+++ collections/fixtures.js
@@ -0,0 +1,106 @@
+testEvt =[
+	{
+	    codeConfirmMail:"ok",
+	    admin:'philippe@canniste.org', //Mail pour l'administration
+	    password:'test',
+	    nom: 'Internationaux de France 2013', //nom de l'évènement
+	    datedeb: '2013-12-13',// Date 
+	    datefin: '2013-12-13',// Date 
+	    forclusion: '2013-11-10', //Date de forclusion
+	    horaires:'Samedi 10h00-19h00, Dimanche 10h00-17h00', //horaires (avec alternatives si pas bordé)
+	    echelle: 'Nationale', //Echelle (nationale, régionale, ...)
+	    cible:'des cannistes !', //Public attendu (->cible)
+	    prix:'30€', //Prix d'inscription
+	    orga: 'Julien Falconnet', //Nom de l'organisateur, 
+	    statut:'en cours', //Statut           
+	    contact:'webmaster@darkhan.net', //Contact (pour information)            
+	    lieu:'Gymnase de Reully, 75012 Paris', //Lieu / adresse
+	    codepostal:'75012', //cp / adresse 
+	    plan:'http://goo.gl/maps/U6l2H', // Plan d’accès
+	    url:'http://www.internationaux2015.com',
+	    programme:'bla bla bla bla bla bla bla', // Programme
+	    options:'buvette, repas sur place', //Options (buvette, offre de repas, etc.)
+	    conditions:'majeur et vacciné', //Conditions de participation
+	    hebergement:'non', //Recommandations d’hébergement
+	    restauration:'non', //Recommandations restauration
+	    visites:'bof' //Recommandations visites
+	},
+	{
+	    codeConfirmMail:"ok",
+	    admin:'philippe@canniste.org', //Mail pour l'administration
+	    password:'test',
+	    nom: 'Championnaat de France 2013', //nom de l'évènement
+	    datedeb: '2014-01-15',// Date 
+	    datefin: '2014-01-15',// Date 
+	    forclusion: '2013-11-10', //Date de forclusion
+	    horaires:'Samedi 10h00-19h00, Dimanche 10h00-17h00', //horaires (avec alternatives si pas bordé)
+	    echelle: 'Nationale', //Echelle (nationale, régionale, ...)
+	    cible:'des cannistes !', //Public attendu (->cible)
+	    prix:'30€', //Prix d'inscription
+	    orga: 'Julien Falconnet', //Nom de l'organisateur, 
+	    statut:'en cours', //Statut           
+	    contact:'webmaster@darkhan.net', //Contact (pour information)            
+	    lieu:'Gymnase de Reully, 75012 Paris', //Lieu / adresse
+	    codepostal:'75012', //cp / adresse 
+	    plan:'http://goo.gl/maps/U6l2H', // Plan d’accès
+	    url:'http://www.internationaux2015.com',
+	    programme:'bla bla bla bla bla bla bla', // Programme
+	    options:'buvette, repas sur place', //Options (buvette, offre de repas, etc.)
+	    conditions:'majeur et vacciné', //Conditions de participation
+	    hebergement:'non', //Recommandations d’hébergement
+	    restauration:'non', //Recommandations restauration
+	    visites:'bof' //Recommandations visites
+	},
+	{
+	    codeConfirmMail:"ok",
+	    admin:'philippe@canniste.org', //Mail pour l'administration
+	    password:'test',
+	    nom: 'Internationaux de France 2013', //nom de l'évènement
+	    datedeb: '2014-05-15',// Date 
+	    datefin: '2014-05-16',// Date 
+	    forclusion: '2013-11-10', //Date de forclusion
+	    horaires:'Samedi 10h00-19h00, Dimanche 10h00-17h00', //horaires (avec alternatives si pas bordé)
+	    echelle: 'Nationale', //Echelle (nationale, régionale, ...)
+	    cible:'des cannistes !', //Public attendu (->cible)
+	    prix:'30€', //Prix d'inscription
+	    orga: 'Julien Falconnet', //Nom de l'organisateur, 
+	    statut:'en cours', //Statut           
+	    contact:'webmaster@darkhan.net', //Contact (pour information)            
+	    lieu:'Gymnase de Reully, 75012 Paris', //Lieu / adresse
+	    codepostal:'75012', //cp / adresse 
+	    plan:'http://goo.gl/maps/U6l2H', // Plan d’accès
+	    url:'http://www.internationaux2015.com',
+	    programme:'bla bla bla bla bla bla bla', // Programme
+	    options:'buvette, repas sur place', //Options (buvette, offre de repas, etc.)
+	    conditions:'majeur et vacciné', //Conditions de participation
+	    hebergement:'non', //Recommandations d’hébergement
+	    restauration:'non', //Recommandations restauration
+	    visites:'bof' //Recommandations visites
+	},
+	{
+	    codeConfirmMail:"13654987",
+	    admin:'philippe@canniste.org', //Mail pour l'administration
+	    password:'test',
+	    nom: 'Titis parisiens 2013', //nom de l'évènement
+	    datedeb: '2014-05-25',// Date 
+	    datefin: '2014-05-26',// Date 
+	    forclusion: '2013-11-10', //Date de forclusion
+	    horaires:'Samedi 10h00-19h00, Dimanche 10h00-17h00', //horaires (avec alternatives si pas bordé)
+	    echelle: 'Nationale', //Echelle (nationale, régionale, ...)
+	    cible:'des cannistes !', //Public attendu (->cible)
+	    prix:'30€', //Prix d'inscription
+	    orga: 'Julien Falconnet', //Nom de l'organisateur, 
+	    statut:'en cours', //Statut           
+	    contact:'webmaster@darkhan.net', //Contact (pour information)            
+	    lieu:'Gymnase de Reully, 75012 Paris', //Lieu / adresse
+	    codepostal:'75012', //cp / adresse 
+	    plan:'http://goo.gl/maps/U6l2H', // Plan d’accès
+	    url:'http://www.internationaux2015.com',
+	    programme:'bla bla bla bla bla bla bla', // Programme
+	    options:'buvette, repas sur place', //Options (buvette, offre de repas, etc.)
+	    conditions:'majeur et vacciné', //Conditions de participation
+	    hebergement:'non', //Recommandations d’hébergement
+	    restauration:'non', //Recommandations restauration
+	    visites:'bof' //Recommandations visites
+	},
+]
