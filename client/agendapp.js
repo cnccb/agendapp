7bd418ac618ac7960d656e48813277165a4ab706
jfalconnet <julien.falconnet@canne-et-dragons.org>
2013-07-23 02:41:57 +0200

    Petit ajustement sur les icones de portée.

--- client/agendapp.js
+++ client/agendapp.js
@@ -70,9 +70,9 @@ Handlebars.registerHelper('iconify', function(text) {
         'definitif':    new Handlebars.SafeString("<i class='icon-ok' title='Evénement confirmé, la date et le lieu ne changeront plus.'></i>"),
         'annule':       new Handlebars.SafeString("<i class='icon-remove' title='Evénement qui n\'aura pas lieu.></i>"),
         'local':        new Handlebars.SafeString("<i class='icon-home' title='Organisation locale'></i>"),
-        'departemental':new Handlebars.SafeString("<i class='icon-star' title='Evénement de portée départementale'></i>"),
-        'regional':     new Handlebars.SafeString("<i class='icon-map-marker' title='Evénement de portée régionale'></i>"),
-        'national':     new Handlebars.SafeString("<i class='icon-road' title='Evénement de portée nationale'></i>"),
+        'departemental':new Handlebars.SafeString("<i class='icon-map-marker' title='Evénement de portée départementale'></i>"),
+        'regional':     new Handlebars.SafeString("<i class='icon-road' title='Evénement de portée régionale'></i>"),
+        'national':     new Handlebars.SafeString("<i class='icon-flag' title='Evénement de portée nationale'></i>"),
         'international':new Handlebars.SafeString("<i class='icon-plane' title='Evénement de portée internationale'></i>"),
     };
     if(inconificationTable[text])
