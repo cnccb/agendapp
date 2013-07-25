ff8797b494513bbf2847227c74edda865c43a788
jfalconnet <julien.falconnet@canne-et-dragons.org>
2013-07-25 11:59:03 +0200

    Fin du support des deux version de dates. fixes #4

--- client/agendapp.js
+++ client/agendapp.js
@@ -411,7 +411,10 @@ Template.nouvelEvt.rendered = function() {
 
 
     //définition des validateur des champs dates
-    //$('#frmNouvelEvt input[type="date"]').attr('pattern', '(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)[0-9][0-9]');
+    $('#frmNouvelEvt input[type="date"]').attr('pattern',
+            '((0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)[0-9]{2})|'
+            +'((19|20)[0-9]{2}[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]))'
+            );
     $('#frmNouvelEvt input[type="date"]').attr('min', getStringFromDate(new Date()));
     var end = new Date();
     end.setFullYear(end.getFullYear() + 3);
